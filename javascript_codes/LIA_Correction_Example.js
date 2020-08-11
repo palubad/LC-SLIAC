54ab0041e775a94e86df7f27fdbc6a70563d3b11
palubad <33784015+palubad@users.noreply.github.com>
2020-08-11 16:59:47 +0200

    Update LIA_Correction_Example.js

--- javascript_codes/LIA_Correction_Example.js
+++ javascript_codes/LIA_Correction_Example.js
@@ -1,36 +1,30 @@
 // call the function
-var LIACorrection = require('users/danielp/LIA_Correction:LIA_Correction_Function');
+var LIACorrection = require('users/danielp/LIA_Correction:LIA_Correction_Function')
 
 // polygon drawed into the Map using the Geometry Tools
 var geometry = ee.Geometry.Polygon(
-        [[[19.67749665195573, 49.01504703386079],
-          [19.67749665195573, 49.014104117442386],
-          [19.67882702762712, 49.014104117442386],
-          [19.67882702762712, 49.01504703386079]]], null, false);
+    [
+        [
+            [19.67749665195573, 49.01504703386079],
+            [19.67749665195573, 49.014104117442386],
+            [19.67882702762712, 49.014104117442386],
+            [19.67882702762712, 49.01504703386079]
+        ]
+    ], null, false);
 
 // Set parameters
 var ROI = geometry,
     startDate = '2019-06-01',
     endDate = '2019-09-01',
-    landCoverType = 312; // 312 for coniferous forest
-// boundingBoxSize, referenceAngle, SARCollection and acquisitionMode are left default
-
-
-// select the Sentinel-1 Image Collection
-var S1Collection = ee.ImageCollection('COPERNICUS/S1_GRD')
-                  .filter(ee.Filter.listContains('transmitterReceiverPolarisation', 'VV'))
-                  .filter(ee.Filter.listContains('transmitterReceiverPolarisation', 'VH'))
-                  .filter(ee.Filter.eq('instrumentMode', 'IW'))
-                  .filterBounds(ROI)
-                  .filterDate(startDate, endDate);
+    landCoverType = 312; // set 312 for coniferous or 311 for broad-leaved forest
+// boundingBoxSize, referenceAngle,  parameters are optional
 
 // Apply the LIA Correction function
 var CorrectedCollection = LIACorrection.LIACorrection(
-                          S1Collection,
-                          ROI,
-                          startDate,
-                          endDate,
-                          landCoverType
+    ROI,
+    startDate,
+    endDate,
+    landCoverType
 );
 
 // Print the resulted Sentinel-1 Image Collection
@@ -38,13 +32,13 @@ print(CorrectedCollection);
 
 // Create Chart of time series before and after correction
 var TSChart = ui.Chart.image.series({
-  imageCollection: CorrectedCollection.select(['VV', 'VH', 'corrected_VV', 'corrected_VH']),
-  region: ROI,
-  reducer: ee.Reducer.mean(),
-  scale: 10,
+    imageCollection: CorrectedCollection.select(['VV', 'VH', 'corrected_VV', 'corrected_VH']),
+    region: ROI,
+    reducer: ee.Reducer.mean(),
+    scale: 10,
 }).setOptions({
     title: 'Time-series of corrected and uncorrected data'
 });
 
-// display the chart in the Console
-print(TSChart)
+// display the TS chart in the Console
+print(TSChart);
