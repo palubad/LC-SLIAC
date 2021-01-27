1452143184ae7909a2ce6ddd3803794502a63a0f
Daniel Paluba <33784015+palubad@users.noreply.github.com>
2021-01-27 13:39:39 +0100

    Added possibility to define the S-1 collection

--- javascript_codes/LC-SLIAC_example.js
+++ javascript_codes/LC-SLIAC_example.js
@@ -2,21 +2,31 @@
 Map.centerObject(geometry, 17);
 
 // call the function 
-var LIACorrection = require('users/danielp/LC-SLIAC:LC-SLIAC')
+var LC_SLIAC = require('users/danielp/LC-SLIAC:LC-SLIAC')
  
 // Set parameters
 var ROI = geometry,
     startDate = '2019-06-01',
     endDate = '2019-09-01',
     landCoverType = 312; // set 312 for coniferous or 311 for broad-leaved forest
-// boundingBoxSize, referenceAngle,  parameters are optional
+
+// Additional S1 collection properties can be set // this parameter is optional
+/*var S1Collection = ee.ImageCollection('COPERNICUS/S1_GRD')
+    .filter(ee.Filter.listContains('transmitterReceiverPolarisation', 'VV'))
+    .filter(ee.Filter.listContains('transmitterReceiverPolarisation', 'VH'))
+    .filter(ee.Filter.eq('instrumentMode', 'IW'))
+    .filterBounds(ROI)
+    .filterDate(startDate, endDate);*/
+
+// boundingBoxSize, referenceAngle, parameters are optional
 
 // Apply the LIA Correction function
-var CorrectedCollection = LIACorrection.LIACorrection(
+var CorrectedCollection = LC_SLIAC.LC_SLIAC(
     ROI,
     startDate,
     endDate,
     landCoverType
+    //S1Collection
 );
 
 // Print the resulted Sentinel-1 Image Collection
